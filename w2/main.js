1d79acf13fef94b1421e382477e6fba1ed2b7c4e
jose lemus <jlemu10@gmail.com>
2024-08-21 21:58:31 -0700

    coding is done

--- w2/main.js
+++ w2/main.js
@@ -1,23 +1,23 @@
 console.log('Hello World!')
 
 // Count the members of your household.
-
+const myMembers = 10;
 // Consider the size of your home. 
-
+const mySize = 10;
 // Evaluate your food choices.
-
+const myChoices = 8;
 // Examine your water consumption.
-
+const myConsumption = 1;
 // Determine how many household purchases you make each year. 
-
+const myPurchases = 6;
 // Consider how much waste you produce.
-
+const myUses = 20;
 // Identify the amount of waste that you recycle.
-
+const myRecycle = 24;
 // Tally up your annual transportation scores. 
-
+const myTransportation = 10; 
 // Add up your points.
-
-
+const cfpTotal = myMembers + mySize + myChoices + myConsumption + myPurchases + myUses + myRecycle + myTransportation 
+// Write JS to update the rendered html (index.html) with total footprints
 
 
