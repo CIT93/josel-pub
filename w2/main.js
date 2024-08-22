be9df267d5376b4c8de6ae5c8fbf6c16bd260aa4
jose lemus <jlemu10@gmail.com>
2024-08-21 21:22:36 -0700

    setup done

--- w2/main.js
+++ w2/main.js
@@ -1,2 +1,23 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+console.log('Hello World!')
+
+// Count the members of your household.
+
+// Consider the size of your home. 
+
+// Evaluate your food choices.
+
+// Examine your water consumption.
+
+// Determine how many household purchases you make each year. 
+
+// Consider how much waste you produce.
+
+// Identify the amount of waste that you recycle.
+
+// Tally up your annual transportation scores. 
+
+// Add up your points.
+
+
+
+
