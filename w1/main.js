f976c381de0cb9f6738b9a5ee2c9cfb48631c589
jose lemus <jlemu10@gmail.com>
2024-08-25 17:09:47 -0700

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,25 @@
-console.log ('Hello World')
+console.log ("hello from inside the main.js")
+
+//let myVar;
+
+const myVar = "Jose";
+const myVarType = typeof (myVar);
+console.log("myVarType " + myVarType); 
+console.log('myVarType ${myVarType}'); 
+
+
+if (myVarType === "number") {
+    console.log(`will 11 this one run?`);
+} else {
+    console.log (`will 13 this one run`);
+} 
+function runNow () {
+    if (myVarType === "number") {
+        console.log(`will 11 this one run?`);
+    } else {
+        console.log (`will 13 this one run`);
+    };
+}
+
+runNow ();
+runNow ();
