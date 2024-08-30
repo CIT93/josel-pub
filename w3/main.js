79a7958b560bdf7e18b523f2789c2ce82e7b5125
jose lemus <jlemu10@gmail.com>
2024-08-29 19:47:52 -0700

    need some help

--- w3/main.js
+++ w3/main.js
@@ -1,27 +1,47 @@
-function determineHouseHoldPts(numberInHousehold) {
-  console.log("Inside the funtion");
-} if (numberInHousehold === 1) {
-  cardonFootprintPoints = cardonFootprintPoints + 14;
-} else if (numberInHousehold === 2) {
-  cardonFootprintPoints = cardonFootprintPoints + 12;
-} else if (numberInHousehold === 3) {
-  cardonFootprintPoints = cardonFootprintPoints + 10;
-} else if (numberInHousehold === 4) {
-  cardonFootprintPoints = cardonFootprintPoints + 8;
-} else if (numberInHousehold === 5) {
-  cardonFootprintPoints = cardonFootprintPoints + 6;
-} else if (numberInHousehold === 6) {
-  cardonFootprintPoints = cardonFootprintPoints + 4;
-} else if (numberInHousehold > 6) {
-  cardonFootprintPoints = cardonFootprintPoints + 2;
+// function determineHouseHoldPts(numberInHousehold) {
+  // console.log("Inside the funtion");
+// } if (numberInHousehold === 1) {
+  // cardonFootprintPoints = cardonFootprintPoints + 14;
+// } else if (numberInHousehold === 2) {
+  // cardonFootprintPoints = cardonFootprintPoints + 12;
+// } else if (numberInHousehold === 3) {
+  // cardonFootprintPoints = cardonFootprintPoints + 10;
+// } else if (numberInHousehold === 4) {
+  // cardonFootprintPoints = cardonFootprintPoints + 8;
+// } else if (numberInHousehold === 5) {
+  // cardonFootprintPoints = cardonFootprintPoints + 6;
+// } else if (numberInHousehold === 6) {
+  // cardonFootprintPoints = cardonFootprintPoints + 4;
+// } else if (numberInHousehold > 6) {
+  // cardonFootprintPoints = cardonFootprintPoints + 2;
 
-  console.log('Based on the number of members in the household of ${numberInHousehold} the points would be ${cardonFootprintPoints}.'); 
-}
+  // console.log('Based on the number of members in the household of ${numberInHousehold} the points would be ${cardonFootprintPoints}.'); 
+// }
 
-let cardonFootprintPoints = 0;
-const numberInHousehold = 3;
+// let cardonFootprintPoints = 0;
+// const numberInHousehold = 3;
 
 // global scope
 
-determineHouseHoldPts(3)
-determineHouseHoldPts (4)
+// determineHouseHoldPts(3)
+// determineHouseHoldPts (4)
+
+
+function determineHouseSizePts(houseSize) {
+  console.log("Inside the function");
+}
+if (houseSize === "large") {
+  cardonFootprintPoints = cardonFootprintPoints + 10;
+} else if (houseSize === "medium") {
+  cardonFootprintPoints = cardonFootprintPoints + 7;
+} else if (houseSize === "small") {
+  cardonFootprintPoints = cardonFootprintPoints + 4;
+} else if (houseSize === apartment) {
+  cardonFootprintPoints = cardonFootprintPoints + 2;
+    
+  console.log('Based on the size of home ${houseSize} the points would be ${cardonFootprintPoints}.');
+}
+
+
+determineHouseSizePts (large)
+determineHouseSizePts (apartment)
