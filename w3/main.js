68f7d7bc933f140faa7bdd4f83b14186b158889a
jose lemus <jlemu10@gmail.com>
2024-08-29 18:02:59 -0700

    setup done

---
+++ w3/main.js
@@ -0,0 +1,24 @@
+function determineHouseHoldPts () {} 
+
+let cardonFootprintPoints = 0;
+const numberInHousehold = 3; 
+
+if (numberInHousehold === 1) {
+    cardonFootprintPoints = cardonFootprintPoints + 14; 
+} else if (numberInHousehold === 2) {
+    cardonFootprintPoints = cardonFootprintPoints + 12; 
+} else if (numberInHousehold === 3) {
+    cardonFootprintPoints = cardonFootprintPoints + 10;
+} else if (numberInHousehold === 4) {
+    cardonFootprintPoints = cardonFootprintPoints +8;
+} else if (numberInHousehold === 5) {
+    cardonFootprintPoints = cardonFootprintPoints +6;
+} else if (numberInHousehold === 6) {
+    cardonFootprintPoints = cardonFootprintPoints +4; 
+} else if (numberInHousehold > 6) {
+    cardonFootprintPoints = cardonFootprintPoints +2; 
+}
+console.log('Based on the number of members in the household of ${numberInHousehold} the points would be ${cardonFootprintPoints}.');
+
+// global scope 
+
