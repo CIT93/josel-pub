163b32862acbef198ee82f564e01e338d67675ca
jose lemus <jlemu10@gmail.com>
2024-08-29 18:40:36 -0700

    functions

--- w3/main.js
+++ w3/main.js
@@ -1,24 +1,27 @@
-function determineHouseHoldPts () {} 
+function determineHouseHoldPts(numberInHousehold) {
+  console.log("Inside the funtion");
+} if (numberInHousehold === 1) {
+  cardonFootprintPoints = cardonFootprintPoints + 14;
+} else if (numberInHousehold === 2) {
+  cardonFootprintPoints = cardonFootprintPoints + 12;
+} else if (numberInHousehold === 3) {
+  cardonFootprintPoints = cardonFootprintPoints + 10;
+} else if (numberInHousehold === 4) {
+  cardonFootprintPoints = cardonFootprintPoints + 8;
+} else if (numberInHousehold === 5) {
+  cardonFootprintPoints = cardonFootprintPoints + 6;
+} else if (numberInHousehold === 6) {
+  cardonFootprintPoints = cardonFootprintPoints + 4;
+} else if (numberInHousehold > 6) {
+  cardonFootprintPoints = cardonFootprintPoints + 2;
+
+  console.log('Based on the number of members in the household of ${numberInHousehold} the points would be ${cardonFootprintPoints}.'); 
+}
 
 let cardonFootprintPoints = 0;
-const numberInHousehold = 3; 
+const numberInHousehold = 3;
 
-if (numberInHousehold === 1) {
-    cardonFootprintPoints = cardonFootprintPoints + 14; 
-} else if (numberInHousehold === 2) {
-    cardonFootprintPoints = cardonFootprintPoints + 12; 
-} else if (numberInHousehold === 3) {
-    cardonFootprintPoints = cardonFootprintPoints + 10;
-} else if (numberInHousehold === 4) {
-    cardonFootprintPoints = cardonFootprintPoints +8;
-} else if (numberInHousehold === 5) {
-    cardonFootprintPoints = cardonFootprintPoints +6;
-} else if (numberInHousehold === 6) {
-    cardonFootprintPoints = cardonFootprintPoints +4; 
-} else if (numberInHousehold > 6) {
-    cardonFootprintPoints = cardonFootprintPoints +2; 
-}
-console.log('Based on the number of members in the household of ${numberInHousehold} the points would be ${cardonFootprintPoints}.');
+// global scope
 
-// global scope 
-
+determineHouseHoldPts(3)
+determineHouseHoldPts (4)
