f7e0c991e7da3f45a8c06a5be23ee1bb6a85634e
jose lemus <jlemu10@gmail.com>
2024-09-08 20:39:50 -0700

    updated output to DOM

--- w4/main.js
+++ w4/main.js
@@ -1,61 +1,86 @@
 const cfpData = [];
+
 function determineHouseSizePts(size) {
-  console.log("inside block scope"); 
-  let houseSizePoints = 0;
+  let houseSize = 0;
   if (size === "large") {
-    houseSizePoints =  10;
+    houseSize =  10;
   } else if (size === "medium") {
-    houseSizePoints = 7;
+    houseSize = 7;
   } else if (size === "small") {
-    houseSizePoints =  4;
+    houseSize =  4;
   } else if (size === "apt") {
-    houseSizePoints = 2;
+    houseSize = 2;
 
       }
-      return houseSizePoints
+      return houseSize
  }
  
    function determineHouseHoldPts(numberInHousehold) {
-   console.log("inside block scope");
    let houseHoldPoints = 0; 
  if (numberInHousehold === 1) {
-  houseHoldPoints = 14
+  houseHoldPoints = 14;
  } else if (numberInHousehold === 2) {
-  houseHoldPoints = 12
+  houseHoldPoints = 12;
  } else if (numberInHousehold === 3) {
-  houseHoldPoints = 10
+  houseHoldPoints = 10;
  } else if (numberInHousehold === 4) {
-  houseHoldPoints = 8
+  houseHoldPoints = 8;
  } else if (numberInHousehold === 5) {
-  houseHoldPoints = 6
+  houseHoldPoints = 6;
  } else if (numberInHousehold === 6) {
-  houseHoldPoints = 4
+  houseHoldPoints = 4;
  } else if (numberInHousehold > 6) {
-  houseHoldPoints = 2
+  houseHoldPoints = 2;
 }
 
 return houseHoldPoints
+  }
 
- }
  // let cardonFootprintPoints = 0;
 
  function start (houseHoldMembers, houseSize) {
-  const houseHoldPts = determineHouseHoldPts(houseHoldMembers) ;
+  const houseHoldPts = determineHouseHoldPts(houseHoldMembers); 
   const houseSizePTS = determineHouseSizePts(houseSize); 
-  const total = "houseHoldPTS" + "houseSizePTS";
+  const total = houseHoldPts + houseSizePTS; 
   cfpData.push(houseHoldMembers, houseSize, houseHoldPts, houseSizePTS, total);
   
  }
 
 function displayOutput () {
+  for(arr of cfpData){
+    console.log(arr)
+    const output = document.getElementById("output"); 
+    const newp = document.createElement("p"); 
+    newp.textContent = `If your household consits of ${arr[0]} members, the score will be ${arr[2]}. 
+    If your housesize is ${arr[1]} you will score ${arr[1]} points. Adding the total Cardon Footprint to ${arr[4]}`; 
+    output.appendChild(newp)
 
+  }
 }
 
  start (5, "apt"); 
  start (4, "large");
  start (3, "meduim");
- start (2, "small");
- start (1, "large");
- start (0, "large");
+ start (2, "small"); 
+ start (1, "apt"); 
+ start (6, "large"); 
+ start (7, "meduim");
+ start (8, "small");
 
-displayOutput () 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+displayOutput() 
